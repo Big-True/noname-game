5eec62ca3ef77f986728ef846763102985aea519
Big_True <big_true@qq.com>
2019-08-01 12:22:34 +0800

    0.0.2

--- main.py
+++ main.py
@@ -1,23 +1,36 @@
 import pygame
 pygame.init()
-logo=pygame.image.load(r'./res/noname-game.ico')
+logo = pygame.image.load(r'./res/noname-game.ico')
+background = pygame.image.load(r'./res/background.png')
 pygame.display.set_icon(logo)
 pygame.display.set_caption('noname game')
 clock = pygame.time.Clock()
-screensize=[800,600]
-screen=pygame.display.set_mode(screensize)
-player_pos=[0,0]
-map=[]
-window=0
-running=True
+screensize = [800, 600]
+screen = pygame.display.set_mode(screensize)
+player_pos = [0, 0]
+map = []
+window = 0
+running = True
 
 while running:
     clock.tick(60)
-    if window==0:
-        pass
-    elif window==1:
-        pass
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running=False
+    screen.fill((0, 0, 0))
+    if window == 0:
+        if screensize[0]/1920 < screensize[1]/1080:
+            background = pygame.transform.scale(
+                background, (screensize[0], int(screensize[0]/16*9)))
+        else:
+            background = pygame.transform.scale(
+                background, (int(screensize[1]/9*16), screensize[1]))
+        screen.blit(background, (0, 0))
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                x, y = event.pos
+                window = 1
+            if event.type == pygame.QUIT:
+                running = False
+    elif window == 1:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
     pygame.display.flip()
