3609668446affe9c736a1b5a26a63c0360f587d4
Big_True <big_true@qq.com>
2019-08-03 12:37:48 +0800

    0.0.4

--- main.py
+++ main.py
@@ -8,9 +8,13 @@ clock = pygame.time.Clock()
 screensize = [800, 600]
 screen = pygame.display.set_mode(screensize)
 player_pos = [0, 0]
-size = 64
-titles = [pygame.transform.scale(
-    pygame.image.load(r'./res/glass.png'), (size, size))]
+size = 32
+speed = 5
+block_imgs = [pygame.transform.scale(
+    pygame.image.load(r'./res/glass.png'), (size, size)),pygame.transform.scale(
+    pygame.image.load(r'./res/player.png'), (size, size))]
+player_img = pygame.transform.scale(
+    pygame.image.load(r'./res/player.png'), (size, size))
 map = []
 window = 0
 running = True
@@ -19,6 +23,7 @@ running = True
 def newmap():
     global map
     map = [[1 for i in range(100)]for j in range(100)]
+    map[0][0]=2
 
 
 while running:
@@ -40,10 +45,23 @@ while running:
             if event.type == pygame.QUIT:
                 running = False
     elif window == 1:
-        for i in range(int(screensize[0]/size)+1):
-            for j in range(int(screensize[1]/size)+1):
-                screen.blit(titles[map[j][i]-1], (i*size, j*size))
+        for i in range(int(player_pos[0]-screensize[0]/2/size), int(player_pos[0]+screensize[0]/2/size)+1):
+            for j in range(int(player_pos[1]-screensize[1]/2/size), int(player_pos[1]+screensize[1]/2/size)+1):
+                if i >= 0 and j >= 0 and i<len(map[0]) and j<len(map):
+                    screen.blit(block_imgs[map[j][i]-1], ((i-player_pos[0])*size +
+                                                          screensize[0]/2, (j-player_pos[1])*size+screensize[1]/2))
+        screen.blit(
+            player_img, ((screensize[0]-size)/2, (screensize[1]-size)/2))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_w:
+                    player_pos[1] -= speed
+                if event.key == pygame.K_a:
+                    player_pos[0] -= speed
+                if event.key == pygame.K_s:
+                    player_pos[1] += speed
+                if event.key == pygame.K_d:
+                    player_pos[0] += speed
     pygame.display.flip()
