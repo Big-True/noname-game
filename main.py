bb2a8fbe30c0674587ad6f4a77075de10f809e27
Big_True <big_true@qq.com>
2019-08-05 21:26:25 +0800

    0.0.7a

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ import blocks
 import chunks
 
 pygame.init()
+
 logo = pygame.image.load(r'./res/noname-game.ico')
-font = pygame.font.SysFont("", 32)
+font = pygame.font.Font("./FZMW.TTF", 32)
 background = pygame.image.load(r'./res/background.png')
 pygame.display.set_icon(logo)
 pygame.display.set_caption('noname game')
@@ -15,7 +16,7 @@ screen = pygame.display.set_mode(screensize)
 player_pos = [0, 0]
 size = 32
 speed = 0.1
-touch_len=5.01
+touch_len = 5.01
 block_imgs = [pygame.transform.scale(
     i.res, (size, size)) for i in blocks.block_res]
 player_img = pygame.transform.scale(
@@ -43,14 +44,15 @@ while running:
             if event.type == pygame.QUIT:
                 running = False
     elif window == 1:
-        pos=font.render("(%.2f,%.2f)"%(player_pos[0],player_pos[1]),True,(0,0,0))
+        pos = font.render("(%.2f,%.2f)" %
+                          (player_pos[0], player_pos[1]), True, (0, 0, 0))
         for i in range(int(player_pos[0]-screensize[0]/2/size)-1, int(player_pos[0]+screensize[0]/2/size)+1):
             for j in range(int(player_pos[1]-screensize[1]/2/size)-1, int(player_pos[1]+screensize[1]/2/size)+1):
                 screen.blit(block_imgs[map.get_block(i, j)], ((i-player_pos[0])*size +
                                                               screensize[0]/2, (j-player_pos[1])*size+screensize[1]/2))
         screen.blit(
             player_img, ((screensize[0]-size)/2, (screensize[1]-size)/2))
-        screen.blit(pos,(0,0))
+        screen.blit(pos, (0, 0))
         key_pressed = pygame.key.get_pressed()
         if key_pressed[pygame.K_w]:
             player_pos[1] -= speed
@@ -63,10 +65,10 @@ while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-            if event.type==pygame.MOUSEBUTTONDOWN:
-                x,y=event.pos
-                if pow(x-screensize[0]/2,2)+pow(y-screensize[1]/2,2)<pow(touch_len*size,2):
-                    t1=(x-screensize[0]/2)/size+player_pos[0]
-                    t2=(y-screensize[1]/2)/size+player_pos[1]
-                    map.set_block(int(t1-t1%1),int(t2-t2%1),0)
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                x, y = event.pos
+                if pow(x-screensize[0]/2, 2)+pow(y-screensize[1]/2, 2) < pow(touch_len*size, 2):
+                    t1 = (x-screensize[0]/2)/size+player_pos[0]
+                    t2 = (y-screensize[1]/2)/size+player_pos[1]
+                    map.set_block(int(t1-t1 % 1), int(t2-t2 % 1), 0)
     pygame.display.flip()
